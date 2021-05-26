02df805ece47adee17bfa963d12f1ed94afb5804
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-26 21:14:48 +0530

    [Shreya] Add: Calculate the length using Cartesian System

--- LineComparisonComputation/LineComparisonComputation/Program.cs
+++ LineComparisonComputation/LineComparisonComputation/Program.cs
@@ -9,7 +9,22 @@ namespace LineComparisonComputation
         {
 
             Console.WriteLine("... Welcome to Line Comparison Computation ... ");
-            Console.ReadLine();
+            
+            Console.WriteLine("Enter x, y coordinates of the line 1::");
+            
+            Console.WriteLine("X1");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("X2");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y1");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y2");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length = new double();
+            Length = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Console.WriteLine("Length of line 1 is:: " + Length);
+            Console.ReadKey();
         }
     }
 
